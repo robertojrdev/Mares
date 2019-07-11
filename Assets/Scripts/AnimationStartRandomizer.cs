b2f21c0b4a5f8517522737de76d74c47a02dced5
Roberto Jr Dev <junior.rmg.rj@gmail.com>
2019-07-11 20:04:33 +0100

    DONE

---
+++ Assets/Scripts/AnimationStartRandomizer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationStartRandomizer : MonoBehaviour
+{
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+        float offset = Random.Range(0f, 1f);
+        anim.SetFloat("cycleoffset", offset);
+    }
+}
